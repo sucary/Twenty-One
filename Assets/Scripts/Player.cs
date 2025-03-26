f13d1505da12e5695263e6b76f4154e5d995c8d6
sucary <78484823+sucary@users.noreply.github.com>
2025-03-26 17:53:17 +0200

    Modify point mechanism

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int Points;
+    public int Points   { get; private set; }
     // string name;
 
 
     /*
-     * May adjust initial points for multi-difficulty
-     */ 
-    public void InitializePlayer()
+     * May adjust initial points for multi-difficulty, see GameManager
+     */
+    public void InitializePlayer(int startingPoints)
     {
-        Points = 10;
+        Points = startingPoints;
     }
 
 
     public void AddToPoints(int value)
     {
         Points += value;
+    }
+
+    public bool IsBusted()
+    {
+        return Points > 21;
     }
 }
